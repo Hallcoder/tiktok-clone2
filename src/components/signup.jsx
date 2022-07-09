46553809f8b8f3fd0e7ffa697c08bf5a9049143f
Hallcoder <hallcoder25@gmail.com>
2022-07-09 08:39:25 +0200

    feature added:show and hide password

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
+import React, { useState , useRef} from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretLeft, faClose } from "@fortawesome/free-solid-svg-icons";
+import {AiFillEye,AiFillEyeInvisible} from 'react-icons/ai';
 import Select1 from "./select";
 import Submit from "../utils/submit"
 import { FormHelperText } from "@mui/material";
 import Joi from "joi";
 function SignUp({ Display, changePage }) {
+  const passwordRef = useRef()
+  const[state,setState] = useState({visible:false})
   const[errors,setErrors] = useState({email:'',username:'',password:'',cpassword:{},year:'',month:'',day:''})
   const[account,setAccount] = useState({
     email:'hallcoder25@gmail.com',
@@ -23,8 +26,10 @@ function SignUp({ Display, changePage }) {
     .required(),
   username:Joi.string().required().max(20),
   password: Joi.string().min(6).max(15).required(),
-  cpassword:Joi.string().min(6).max(15).required()
-
+  cpassword:Joi.string().min(6).max(15).required(),
+  day:Joi.number().required(),
+  month:Joi.number().required(),
+  year:Joi.number().required()
   })
   const months = [
     "January",
@@ -57,10 +62,11 @@ function SignUp({ Display, changePage }) {
   const {error} = schem.validate(obj)
   return error ? error:{};
  }
- const handleSelectChange = (e) => {
- v
- }
- const handleChange = ({currentTarget:input})=>{
+const handleShowPassword = () => {
+ setState({visible:!state.visible});
+ passwordRef.current.type = passwordRef.current.type === 'password' ? 'text' : 'password';
+}
+ const handleChange = ({target:input})=>{
    const errors1 = {...errors};
    const account1 ={...account};
   if(input.name === 'cpassword' ) {
@@ -77,7 +83,6 @@ function SignUp({ Display, changePage }) {
     setErrors(errors1);
     setAccount(account1);
   }
-  console.log(account)
   }
   const handleSubmit = (e) =>{
   e.preventDefault();
@@ -106,9 +111,9 @@ function SignUp({ Display, changePage }) {
         <h1 className="text-center font-bold text-2xl">Sign up</h1>
         <form className="flex flex-col justify-around" onSubmit={(e) => handleSubmit(e)}>
           <div className="flex flex-row justify-around">
-            <Select1 data={months} name='month' value={month}  onChange={handleSelectChange} label={"month"}  />
-            <Select1 data={days} name='day' value={day} onChange={handleSelectChange} label={"day"} />
-            <Select1 data={years} name='year' value={year} onChange={handleSelectChange} label={"year"} />
+            <Select1 data={months} name='month' value={month}  onChange={handleChange} label={"month"}  />
+            <Select1 data={days} name='day' value={day} onChange={handleChange} label={"day"} />
+            <Select1 data={years} name='year' value={year} onChange={handleChange} label={"year"} />
           </div>
           <div className="ml-10">
             <FormHelperText className="w-9/12">
@@ -116,14 +121,14 @@ function SignUp({ Display, changePage }) {
             </FormHelperText>
           </div>
           <div>
-            <div  className="w-10/12 flex flex-col bg-gray-100 h-12 m-auto items-center ">
+            <div  className="w-10/12 flex flex-row bg-gray-100 h-12 m-auto items-center ">
             <input
               type="email"
               name="email"
               value ={email}
               onChange={handleChange}
               placeholder="Email ..."
-              className="w-full inline-block border-2 indent-2 rounded-sm  h-full"
+              className="w-full inline-block border-2  indent-2 rounded-sm  h-full"
             />
             </div>
             <div className='h-4/6 w-full'>
@@ -146,22 +151,24 @@ function SignUp({ Display, changePage }) {
             </div>
           </div>
           <div>
-            <div className="w-10/12 flex flex-col  border-2 h-12 ml-auto mr-auto mt-4 items-center ">
+            <div className="w-10/12 flex flex-row  border-2 h-12 ml-auto mr-auto mt-4 items-center ">
               <input
                 type="password"
                 value={password}
                 name="password"
+                ref={passwordRef}
                 onChange={handleChange}
                 placeholder="Password..."
                 className="w-full inline-block indent-2 rounded-sm  h-full"
               />
+            <span className="flex w-2/12 border-l-2 h-full justify-center text-xl items-center">{state.visible ? <AiFillEyeInvisible className='text-center' onClick={handleShowPassword}/>:<AiFillEye className="text-center" onClick={handleShowPassword}/>}</span>
             </div>
                <div className='h-4/6 w-full'>
               {errors.password.message && (<div class="h-8 m-auto w-10/12 rounded-sm text-red-500 bg-red-200">{errors.password.message}</div>)}
             </div>
           </div>
           <div>
-            <div className="w-10/12 flex  flex-col bg-gray-100 border-2 h-12 ml-auto mr-auto mt-4 items-center ">
+            <div className="w-10/12 flex  bg-gray-100 border-2 h-12 ml-auto mr-auto mt-4 items-center ">
               <input
                 type="password"
                 name="cpassword"
@@ -170,6 +177,7 @@ function SignUp({ Display, changePage }) {
                 placeholder="Confirm Password..."
                 className="w-full indent-2 rounded-sm  h-full"
               />
+            {/* <span className="flex w-2/12 border-l-2 h-full justify-center text-xl items-center">{state.visible ? <AiFillEyeInvisible className='text-center' onClick={handleShowPassword}/>:<AiFillEye className="text-center" onClick={handleShowPassword}/>}</span> */}
             </div>
                <div className='h-4/6 w-full'>
               {errors.cpassword.message && (<div class="h-2/6 m-auto w-10/12 rounded-sm text-red-500 bg-red-200">{errors.cpassword.message}</div>)}
